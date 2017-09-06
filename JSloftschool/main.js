3a622e5b4f778395e5d465d8480462e19c18b538
Mikeb <sweepjke205@gmail.com>
2017-09-06 17:16:53 +0300

    JSloftschool#2

--- JSloftschool/main.js
+++ JSloftschool/main.js
@@ -30,7 +30,42 @@ document.querySelectorAll('.class1'); //найти все элементы, по
 	nodeType тип узла(текст, элемент, комментарий и т.д)
 	nodeValue для узлов типа "текст" и "комментарии", хранит их содержимое
 	(trim() для работы со строками убирает в начале и в конце пробелы).
+
 	textContent все дочерние текстовые узлы, объединенные вместе.
 	document.createElement('элемент'); создание элементов при помощи JS
+
+	appendChild(child) добавить потомка у узла (в конце узла)
+	insertBefore(what,before) добавить потомка в узел, перед другим потомком внутри узла
+
+	remove() удалить со страници элемент
+	removeChild(child) удалить дочерний узел	
 	
+	innerHTML html-код содержимое текущего элемента
+	outerHTML html-код и элемента и его содержимого	
+
+	style позволяет добавить/удалять/ просматривать css-стили элемента
+
+	tagname -имя тега узла
+
+	getAttribute(name) получить значение атрибута по его имени
+	getAttribute(name, value) установить значение атрибута по его имени
+	removeAttribute(name) удалить атрибут по его имени
+*/
+
+
+/*
+			Примеры
+
+var newDiv = document.createElement('div');
+
+newDiv.textContent = "new DIV!";
+
+container.appendChild(newDiv);
+container.insertBefore(newDiv, container.firstChild);
+
+d2.remove();
+container.removeChild(d1);
+container.innerHTML;
+container.innerHTML = "<div>new div</div>"; стереть все и запить новую информацию
+сontainer.outerHTML;
 */
